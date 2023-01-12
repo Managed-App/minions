4a0bdc48094d9d45d909611939016c10073bc7ea
Rupert Señga <pertsenga@gmail.com>
2023-01-12 14:33:35 +0800

    Create function to check concurrent deployments to same env

--- util.js
+++ util.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 function stripAnsi(raw) {
     return raw.replace(
         /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '');
@@ -15,4 +17,19 @@ function wait(ms) {
     })
 }
 
-module.exports = {wait, stripAnsi, wrapMarkdownCode};
+const attemptDeployToEnv = async (envName, actorName, deploymentPromiseCallback) => {
+    return new Promise((resolve, reject) => {
+        if (_.has(global.deploymentState, envName)) {
+            reject(`Env ${env} deployment triggered by ${actorName} is in progress, please try again later.`)
+        }
+
+        _.set(global.deploymentState, envName, { command: 'deploy', user: actorName })
+
+        deploymentPromiseCallback
+            .then(() => _.unset(global.deploymentState, envName))
+            .then(() => resolve())
+            .catch((error) => reject(error))
+    })
+}
+
+module.exports = {wait, stripAnsi, wrapMarkdownCode, attemptDeployToEnv};
