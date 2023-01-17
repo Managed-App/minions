d6211bcd93d89c834cca44d2066b94bb62d225ab
Rupert Señga <pertsenga@gmail.com>
2023-01-17 16:16:44 +0800

    Convert execSync commands to async

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-const {exec, execSync} = require("child_process");
+const {exec} = require("child_process");
 const util = require('util')
 const {stripAnsi, wait, attemptDeployToEnv} = require("./util");
 const { ConcurrentDeploymentError } = require('./errors');
@@ -61,7 +61,7 @@ const messageCurrentDeployerAboutAttemptedDeployment = async (client, envName, a
 
 async function runDeisReleasesList(target, log) {  //needed for ruby2.6.4
     const command = `${process.env.DEIS_HOME}/deis releases:list -a managed-${target}`;
-    const resp = execSync(command, {
+    const stdout = await execPromise(command, {
         env: {
             ...process.env,
             PATH: process.env.RUBY_PATH + ":$PATH",
@@ -69,9 +69,9 @@ async function runDeisReleasesList(target, log) {  //needed for ruby2.6.4
             GEM_HOME: process.env.GEM_HOME,
         },
         cwd: `${process.env.MANAGED_HOME}`,
-    });
+    }).then(res => res.stdout);
     log.debug(`os executed ${command}`);
-    var depls = stripAnsi(resp.toString("utf8")).split("\n");
+    var depls = stripAnsi(stdout.toString("utf8")).split("\n");
     depls = depls.filter((line) => line.includes("deployed"));
     const version = depls[0].slice(depls[0].lastIndexOf(":") + 1);
     log.info(`env ${target} running version ${version}`);
@@ -92,7 +92,7 @@ async function runSkipperDeploy(target, version, respond, log) {
             },
             cwd: `${process.env.MANAGED_HOME}`,
         })
-            .then(resp => log.debug(`os executed ${command} stdout: ${resp}`))
+            .then(resp => log.debug(`os executed ${command} stdout: ${resp.stdout}`))
             .then(async () => {
                 let isSuccess = false;
                 for (let i = 0; i < 100; i++) {
